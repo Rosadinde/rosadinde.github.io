831777a6962775d6b7aa77722f11093ac3fe118c
Rosadinde <neeira@live.fr>
2018-09-26 22:37:21 +0200

    avec les dix secondes

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 var booty = "Kikoo clique sur ok pour venir voir du booty"
 
-
-
-if(confirm(booty)) {
-    location.href=('https://www.youtube.com/watch?v=dQw4w9WgXcQ') 
-    //ouverture rickroll
-}
-else {
-    
-    //on reste sur le site
-}
+setTimeout(function(){ 
+  confirm(booty); 
+  if(booty == true) {
+    location.href=('https://www.youtube.com/watch?v=dQw4w9WgXcQ')   
+  }
+}, 10000);
