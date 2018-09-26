41c87f33e64df3edb0d4b775879d6ff857db72eb
Rosadinde <neeira@live.fr>
2018-09-26 20:57:52 +0200

    bettermotherfucking avec popup booty

---
+++ main.js
@@ -0,0 +1,12 @@
+var booty = "Kikoo clique sur ok pour venir voir du booty"
+
+
+
+if(confirm(booty)) {
+    location.href=('https://www.youtube.com/watch?v=dQw4w9WgXcQ') 
+    //ouverture rickroll
+}
+else {
+    
+    //on reste sur le site
+}
